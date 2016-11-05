eab075b794443990c0e0a824be736f5fd86b8ee4
UlisesFS-ISC <dantedreadnaught@gmail.com>
2016-11-05 15:17:52 -0600

    Introducing morgan and jsonparser

--- server.js
+++ server.js
@@ -19,16 +19,21 @@ mongoose.connect(DATABASE_URL); // connect to database
 app.set('superSecret', 'blundell'); // secret variable
 
 
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// use morgan to log requests to the console
+app.use(morgan('dev'));
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
 app.listen(port, function(){
-  console.log("Express server listening on port: " + port);
+  console.log("Express server listening on port:" + port);
 });
 
 app.get('/', function(req,res){
-	res.send('Hello this API is at '+ port+'/api');
+	res.send('Hello this API is at  change2'+ port+'/api');
 });

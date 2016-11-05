b0c334c45bb5b54cc32f7942bcb4aa9c5b7b7774
UlisesFS-ISC <dantedreadnaught@gmail.com>
2016-11-05 15:10:51 -0600

    Introducing the database 1

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ var port = process.env.PORT || 8080;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+var databaseaddress= "mongodb://Ulises:timerbolt4real@ds143767.mlab.com:43767/timerbolt";
+mongoose.connect(databaseaddress); // connect to database
+app.set('superSecret', 'blundell'); // secret variable
+
+
+
+
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 

d075b63a617ead43134d570376bcfd62dc1d4215
UlisesFS-ISC <dantedreadnaught@gmail.com>
2018-10-15 23:56:13 -0500

    initial commit v0.0.9

--- app/models/schedule-entry.js
+++ app/models/schedule-entry.js
@@ -1,88 +1,46 @@
-
 var mongoose = require('mongoose');
-
-
 var Schema= mongoose.Schema;
 
-
-
 var ScheduleEntrySchema = new Schema({
-
     uuid: {
-
         type:String,
-
         required:true
-
     },
-
     scheduleEntryName:{
-
         type: String,
-
         required: true
-
     },
-
     userName:{
-
         type:String,
-
         required:true
-
     },
-
     activeInDays: {
-
-        type: String,
-
-        enum: [ 'SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'],
-
-        default: 'SUNDAY',
-
+        type: [String],
+        default: ['SUNDAY'],
         required: false
-
     },
-
     scheduledDayTime:{ //SpecificDate and dayTime if singleUse === true ? //SpecificDate , else daytime
-
-        type: [Date],
-
-        default: undefined,
-
-        required:true
-
-    },
-
-    datePosted: {
-
-        type:Date,
-
-        required:true
-
-    },
-
-    singleUse:{
-
-        type: Boolean,
-
-        required: true
-
-    },
-
-    activityTag: {
-
         type: String,
-
+        required: false
+    },
+    datePosted: {
+        type: String,
+        required: false
+    },
+    singleUse:{
+        type: Boolean,
+        required: true
+    },
+    activityTag: {
+        type: String,
         enum: ['PRODUCTIVITY', 'LEISURE', 'SKILL_LEARNING', 'ERRAND', 'IMPORTANT'],
-
         default: 'LEISURE'
     }
-    });
+});
 
 ScheduleEntrySchema.pre('save',function(next){
-	var ScheduleEntrySchema=this;
-	return next();
+    var ScheduleEntrySchema=this;
+    return next();
 });
 
 
